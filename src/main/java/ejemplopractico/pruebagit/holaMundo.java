09d7217573fd3df6fec61f2a3341d66deaf389ce
diegoferpit <diego.ferpit@outlook.es>
2021-12-13 19:36:46 +0100

    Implementada la clase principal

---
+++ src/main/java/ejemplopractico/pruebagit/holaMundo.java
@@ -0,0 +1,16 @@
+package ejemplopractico.pruebagit;
+
+/**
+ *
+ * @author Diego Fernandez Pita
+ */
+public class holaMundo extends javax.swing.JFrame {
+    
+    public static void main(String[] args) {
+        holaMundo app = new holaMundo();
+        app.setSize(300,200);
+        app.setTitle("Hola mundo");
+        app.setVisible(true);
+    }
+    
+}
