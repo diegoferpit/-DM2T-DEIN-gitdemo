249764357d6b8476c9eb7b3387bddc4936b36acc
diegoferpit <diego.ferpit@outlook.es>
2021-12-13 20:13:37 +0100

    holaMundo atributo añadido

--- src/main/java/ejemplopractico/pruebagit/holaMundo.java
+++ src/main/java/ejemplopractico/pruebagit/holaMundo.java
@@ -6,10 +6,12 @@ package ejemplopractico.pruebagit;
  */
 public class holaMundo extends javax.swing.JFrame {
     
+    int secreto;
+    
     public static void main(String[] args) {
         holaMundo app = new holaMundo();
         app.setSize(300,200);
-        app.setTitle("Hola mundo");
+        app.setTitle("Hola mundo secreto");
         app.setVisible(true);
     }
     
